f95c1fd54bf346dcd55e4630694e22e9998e446c
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2025-05-20 01:26:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b
 #--------------------------------------------
 #HELP_TXT = "<b><blockquote>ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋ ғᴏʀ @Nova_Flix\n\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n├/about : ᴏᴜʀ Iɴғᴏʀᴍᴀᴛɪᴏɴ\n└/help : ʜᴇʟᴘ ʀᴇʟᴀᴛᴇᴅ ʙᴏᴛ\n\n sɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴏɴ ʟɪɴᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ᴊᴏɪɴ ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴛʜᴀᴛs ɪᴛ.....!\n\n ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ <a href=https://t.me/cosmic_freak>sᴜʙᴀʀᴜ</a></blockquote></b>"
 HELP_TXT = """
-<blockquote expandable>
+<blockquote>
 ❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs
 ├ /start : ᴍᴜʟᴀɪ ʙᴏᴛ ᴀᴛᴀᴜ ᴅᴀᴘᴀᴛᴋᴀɴ ᴘᴏsᴛɪɴɢᴀɴ
 ├ /batch : ʙᴜᴀᴛ ᴛᴀᴜᴛᴀɴ ᴜɴᴛᴜᴋ ʟᴇʙɪʜ ᴅᴀʀɪ sᴀᴛᴜ ᴘᴏsᴛɪɴɢᴀɴ
