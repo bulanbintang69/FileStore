1886defc6135519d339635e485bb7845570de61e
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2025-05-19 07:43:47 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -158,8 +158,7 @@ async def start_command(client: Client, message: Message):
     ]
             ]
         )
-        await message.reply_photo(
-            photo=START_PIC,
+        await message.reply_text(
             caption=START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
