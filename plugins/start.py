b90e16ab2b622095e2223213459aef7b6b053ade
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2025-05-19 07:47:45 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -159,7 +159,7 @@ async def start_command(client: Client, message: Message):
             ]
         )
         await message.reply_text(
-            caption=START_MSG.format(
+            START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
                 username=None if not message.from_user.username else '@' + message.from_user.username,
