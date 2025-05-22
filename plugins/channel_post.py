c503c1bdd255005c3b5886ae1a1f66d5c7d0a1fc
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2025-05-22 09:22:02 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -11,24 +11,35 @@ from helper_func import encode, admin
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please Wait...!", quote = True)
     try:
-        temp_message = await message.copy(chat_id = client.db_channel.id)
-        post_message = await message.copy(chat_id = client.db_channel.id, caption = f"https://t.me/{client.username}?start={await encode(f'get-{temp_message.id * abs(client.db_channel.id)}')}", disable_notification=True)
+        temp_message = await client.send_message(client.db_channel.id, "temp")
+        post_message_id = temp_message.id
         await temp_message.delete()
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        temp_message = await message.copy(chat_id = client.db_channel.id)
-        post_message = await message.copy(chat_id = client.db_channel.id, caption = f"https://t.me/{client.username}?start={await encode(f'get-{temp_message.id * abs(client.db_channel.id)}')}", disable_notification=True)
+        temp_message = await client.send_message(client.db_channel.id, "temp")
+        post_message_id = temp_message.id
         await temp_message.delete()
     except Exception as e:
         print(e)
         await reply_text.edit_text("Something went Wrong..!")
         return
 
-    converted_id = post_message.id * abs(client.db_channel.id)
+    converted_id = post_message_id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+
+    try:
+        post_message = await message.copy(chat_id = client.db_channel.id, caption = f"Link Download = {link}", disable_notification=True)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        post_message = await message.copy(chat_id = client.db_channel.id, caption = f"Link Download = {link}", disable_notification=True)
+    except Exception as e:
+        print(e)
+        await reply_text.edit_text("Something went Wrong..!")
+        return
+
     await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
