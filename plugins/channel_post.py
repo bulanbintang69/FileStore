8ff63b18fba3fa0948eba0a93c2f41c014b9cbc1
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2025-05-22 10:04:09 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -4,17 +4,18 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 from bot import Bot
-from config import *
 from helper_func import encode, admin
+
+DOWNLOAD_CHANNEL_ID = -1002233565278  # ganti dengan ID channel download
 
 @Bot.on_message(filters.private & admin & ~filters.command(['start', 'commands','users','broadcast','batch', 'custom_batch', 'genlink','stats', 'dlt_time', 'check_dlt_time', 'dbroadcast', 'ban', 'unban', 'banlist', 'addchnl', 'delchnl', 'listchnl', 'fsub_mode', 'pbroadcast', 'add_admin', 'deladmin', 'admins']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Tunggu", quote = True)
     try:
-        post_message = await message.copy(chat_id = client.db_channel.id, caption = "ini caption contoh", disable_notification=True)
+        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        post_message = await message.copy(chat_id = client.db_channel.id, caption = "ini caption contoh", disable_notification=True)
+        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
         await reply_text.edit_text("Something went Wrong..!")
@@ -24,10 +25,16 @@ async def channel_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("📥 Save File", url=f'{link}')]])
 
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    try:
+        download_message = await message.copy(chat_id = DOWNLOAD_CHANNEL_ID, caption = f"ini caption contoh\n\n{link}", reply_markup=reply_markup, disable_notification=True)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        download_message = await message.copy(chat_id = DOWNLOAD_CHANNEL_ID, caption = f"ini caption contoh\n\n{link}", reply_markup=reply_markup, disable_notification=True)
+    except Exception as e:
+        print(e)
+        await reply_text.edit_text("Something went Wrong..!")
+        return
 
-    if not DISABLE_CHANNEL_BUTTON:
-        await post_message.edit_reply_markup(reply_markup)
+    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
